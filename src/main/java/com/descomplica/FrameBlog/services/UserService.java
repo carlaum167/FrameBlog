535c716562abdfdc715812b5e91b07d6f4c48ee8
carlaum167 <carlosgado167@gmail.com>
2026-04-24 15:39:12 -0300

    finished endpoints of User Controller

--- src/main/java/com/descomplica/FrameBlog/services/UserService.java
+++ src/main/java/com/descomplica/FrameBlog/services/UserService.java
@@ -1,4 +1,17 @@
 package com.descomplica.FrameBlog.services;
 
+import com.descomplica.FrameBlog.modelos.User;
+
+import java.util.List;
+
 public interface UserService {
+    User save(User user);
+
+    List<User> getAll();
+
+    User get(Long id);
+
+    User update(Long id, User user);
+
+    void delete(Long id);
 }
