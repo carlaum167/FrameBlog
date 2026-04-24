44e9d7895d96b746587f476e527129475f57bdaa
carlaum167 <carlosgado167@gmail.com>
2026-04-24 16:37:02 -0300

    implementation in UserServiceImpl

--- src/main/java/com/descomplica/FrameBlog/services/impl/UserServiceImpl.java
+++ src/main/java/com/descomplica/FrameBlog/services/impl/UserServiceImpl.java
@@ -1,4 +1,25 @@
 package com.descomplica.FrameBlog.services.impl;
 
-public class UserServiceImpl {
-}
+import com.descomplica.FrameBlog.modelos.User;
+import com.descomplica.FrameBlog.repositories.UserRepository;
+import com.descomplica.FrameBlog.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserRepository
+
+    @Override
+    public User save(final User){
+        User existingUser = UserRepository.findByUsername(User.getName());
+        if (Objects.nonNull(existingUser)){
+            throw new RuntimeException("Existing User");
+        }
+        User entity = new User(user.getUserId(), user.getName(), user.getEmail(), user.getPassword(), user.getRole());
+        User newUser = UserRepository.save(entity);
+        return new User(newUser.getUserId(), newUser.getName(), newUser.getEmail(), newUser.getPassword(), newUser.getRole());
+    }
