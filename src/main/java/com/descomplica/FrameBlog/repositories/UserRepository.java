03871dd66bd96077c0166240d9ce0bad910d5f82
carlaum167 <carlosgado167@gmail.com>
2026-04-24 16:21:32 -0300

    created initial method of UserRepository

--- src/main/java/com/descomplica/FrameBlog/repositories/UserRepository.java
+++ src/main/java/com/descomplica/FrameBlog/repositories/UserRepository.java
@@ -1,4 +1,12 @@
 package com.descomplica.FrameBlog.repositories;
 
-public class UserRepository {
+
+import com.descomplica.FrameBlog.modelos.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String login);
 }
